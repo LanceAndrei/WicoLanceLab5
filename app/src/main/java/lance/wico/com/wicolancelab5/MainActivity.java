cf33bffa8039d857a0726656b5174850934bba80
LanceAndrei <2015084992@ust-ics.mygbiz.com>
2018-10-20 19:02:26 +0800

    Initial commit

---
+++ app/src/main/java/lance/wico/com/wicolancelab5/MainActivity.java
@@ -0,0 +1,42 @@
+package lance.wico.com.wicolancelab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button actOne = (Button) findViewById(R.id.actone);
+        actOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SecondActivity = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(SecondActivity);
+
+            }
+        });
+
+        Button mapButton = (Button) findViewById(R.id.mapone);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:14.625217, 121.001973");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+
+            }
+        });
+    }
+}
